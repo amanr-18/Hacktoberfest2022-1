113a555dd6698674a528bc7eea117865802dcf57
amanr-18 <114903711+amanr-18@users.noreply.github.com>
2022-10-28 10:36:05 +0530

    Create Different String.py

---
+++ Different String.py/Different String/Different String.py
@@ -0,0 +1,22 @@
+# cook your dish here
+
+t=int(input())
+
+for i in range(t):
+
+    n = int(input())
+
+    for i in range(n):
+
+        s=input()
+
+        if s[i]=='1':
+
+            print('0',end="")
+
+        else:
+
+            print('1',end="")
+
+    print()       
+
